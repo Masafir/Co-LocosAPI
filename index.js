4b3cccaa8ced6edadaf9a1d2125e90956b4de0d4
Cédric BOISSINOT <cedric.boissinot@tgs-france.fr>
2021-02-18 13:06:38 +0100

    add task v1

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
 var express = require('express');
 var cors = require('cors');
+var bodyParser = require('body-parser');
 
+var fs = require('fs');
 
 const colocs = require('./coloc.json');
 
+// create application/json parser
+var jsonParser = bodyParser.json()
+ 
+// create application/x-www-form-urlencoded parser
+var urlencodedParser = bodyParser.urlencoded({ extended: false })
 
 var app = express();
 app.use(cors());
@@ -15,15 +22,91 @@ app.get('/getcolocs',(req,res)=>{
     res.send(colocs);
 });
 
-app.post("/tasks", (req, res)=>{
-    let taskSearch = colocs.find(e => e.tasks)
-    colocs.push(req.body);
-    res.status(200)
+app.post("/task", jsonParser, (req, res, err)=>{
+    // colocs.push(req.body);
+    try {
+        console.log(req)
+        let colocSearch = colocs.find(e => e.name == req.body.coloc);
+        let colocIndex = colocs.findIndex(t => t.name == req.body.coloc);
+        let newGroupColoc = [...colocs];
+        console.log(colocSearch)
+        if(colocSearch && colocSearch != undefined){
+            let taskIndex = colocSearch.tasks.findIndex(t => t.value == req.body.task.value)
+            let newTask = {}
+            if (taskIndex > 0){
+                let taskSearch = colocSearch.tasks[taskIndex];
+                newTask = req.body.task;
+                colocSearch.tasks.splice(taskIndex, 1, newTask);
+                newGroupColoc.splice(colocIndex,1, colocSearch);
+                let data = JSON.stringify(newGroupColoc);
+                fs.writeFileSync('./coloc.json', data, err => {
+                    console.log(err);
+                })
+            }
+            else {
+                newTask = req.body.task;
+                colocSearch.tasks.push(newTask);
+                newGroupColoc.splice(colocIndex,1, colocSearch);
+                let data = JSON.stringify(newGroupColoc);
+                fs.writeFileSync('./coloc.json', data, err => {
+                    console.log(err);
+                })
+            }
+        }
+        else {
+            return ("no coloc")  
+        }
+
+        res.send("ok")
+        res.status(200)
+    }
+    catch(error){
+        return res.status(400).json({ error: error.toString() });
+    }
+
 
 });
 
-app.post("/user", (req, res)=>{
-    
+app.post("/user", jsonParser (req, res)=>{
+    try {
+        console.log(req)
+        let colocSearch = colocs.find(e => e.name == req.body.coloc);
+        let colocIndex = colocs.findIndex(t => t.name == req.body.coloc);
+        let newGroupColoc = [...colocs];
+        console.log(colocSearch)
+        if(colocSearch && colocSearch != undefined){
+            let taskIndex = colocSearch.tasks.findIndex(t => t.value == req.body.task.value)
+            let newUser = {}
+            if (taskIndex > 0){
+                let taskSearch = colocSearch.tasks[taskIndex];
+                newTask = req.body.task;
+                colocSearch.tasks.splice(taskIndex, 1, newTask);
+                newGroupColoc.splice(colocIndex,1, colocSearch);
+                let data = JSON.stringify(newGroupColoc);
+                fs.writeFileSync('./coloc.json', data, err => {
+                    console.log(err);
+                })
+            }
+            else {
+                newTask = req.body.task;
+                colocSearch.tasks.push(newTask);
+                newGroupColoc.splice(colocIndex,1, colocSearch);
+                let data = JSON.stringify(newGroupColoc);
+                fs.writeFileSync('./coloc.json', data, err => {
+                    console.log(err);
+                })
+            }
+        }
+        else {
+            return ("no coloc")  
+        }
+
+        res.send("ok")
+        res.status(200)
+    }
+    catch(error){
+        return res.status(400).json({ error: error.toString() });
+    }
 });
 
 app.post("/users", (req, res)=>{
